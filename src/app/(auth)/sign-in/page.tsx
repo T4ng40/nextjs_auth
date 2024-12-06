48e5170580e896a885aa9b35e2061ad2fee7cbff
tangodev <calebetango123@gmail.com>
2024-12-06 00:11:01 -0300

    Revert "auth routers and dashboard page"
    This reverts commit 8d101df7b42ee13acbad69c918517f1f75a85f8d.

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,65 +1,17 @@
-'use client'
+import Link from "next/link";
 
-import Link from 'next/link'
-
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { z } from 'zod'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import {
-  FormField,
-  FormControl,
-  FormDescription,
-  FormMessage,
-  FormItem,
-  FormLabel,
-  Form
-} from '@/components/ui/form'
-import { useState } from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import { toast } from 'sonner'
-
-const schema = z.object({
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(1, 'Password is required')
-})
-
-type FormData = z.infer<typeof schema>
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function SignIn() {
-  const [isLoading, setIsLoading] = useState(false)
-  const router = useRouter()
-  const form = useForm<FormData>({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      email: '',
-      password: ''
-    }
-  })
-
-  const handleSubmit = form.handleSubmit(async (formData) => {
-    try {
-      setIsLoading(true)
-
-      await axios.post('/api/auth/sign-in', formData)
-
-      router.push('/')
-    } catch {
-      toast.error('Invalid credentials')
-      setIsLoading(false)
-    }
-  })
-
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -69,66 +21,39 @@ export default function SignIn() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Form {...form}>
-          <form className="grid gap-4" onSubmit={handleSubmit}>
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>E-mail</FormLabel>
-                  <FormControl>
-                    <Input
-                      autoComplete="email"
-                      placeholder="email@gmail.com"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
             />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex items-center">
-                    <FormLabel htmlFor="password">Password</FormLabel>
-                    <Link
-                      href="#"
-                      className="ml-auto inline-block text-sm underline"
-                    >
-                      Forgot your password?
-                    </Link>
-                  </div>
-                  <FormControl>
-                    <Input
-                      autoComplete="current-password"
-                      placeholder="*****"
-                      type="password"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="w-full" disabled={isLoading}>
-              {!isLoading ? 'Login' : 'Loading...'}
-            </Button>
-            <Button variant="outline" className="w-full" type="button">
-              Login with Google
-            </Button>
-          </form>
-        </Form>
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </div>
         <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{' '}
-          <Link href="/sign-up" className="underline">
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="underline">
             Sign up
           </Link>
         </div>
       </CardContent>
     </Card>
-  )
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,21 @@
-import { Toaster } from '@/components/ui/sonner'
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 
-import './globals.css'
-
-import { ThemeProvider } from '../components/theme-provider'
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Nextjs | Auth',
-  description: 'Authentication with Next.js'
-}
+  title: "Nextjs | Auth",
+  description: "Authentication with Next.js",
+};
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <>
-      <html lang="pt-br" suppressHydrationWarning>
-        <body className="antialised">
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <Toaster />
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
-  )
+    <html lang="pt-br">
+      <body className="antialised">{children}</body>
+    </html>
+  );
 }
